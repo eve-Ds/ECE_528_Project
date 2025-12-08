80ea5996493e0e2b39558b5955c45405932cbd4c
eve-Ds <lyneve867@gmail.com>
2025-12-08 14:10:41 -0800

    Updated Files

--- BLE_UART/main.c
+++ BLE_UART/main.c
@@ -38,23 +38,30 @@ void Process_BLE_UART_Data(char BLE_UART_Buffer[])
     if (Check_BLE_UART_Data(BLE_UART_Buffer, "!B51"))
     {
         Motor_Forward(1500,2000);
+        Nokia5110_DrawFullImage(Forward_Face);
+
     }
     //when up arrow is released robot stops
     else if (Check_BLE_UART_Data(BLE_UART_Buffer, "!B50"))
     {
         Motor_Stop();
+        Nokia5110_Clear();
+        Nokia5110_DrawFullImage(Ovo_face);
+
     }
 
     //when down arrow is pressed robot moves backward
     else if (Check_BLE_UART_Data (BLE_UART_Buffer, "!B61"))
     {
         Motor_Backward(900,1505);
+        Nokia5110_DrawFullImage(Looking_Back);
 
     }
     //when down arrow is released robot moves backward
     else if (Check_BLE_UART_Data (BLE_UART_Buffer, "!B60"))
     {
         Motor_Stop();
+        Nokia5110_DrawFullImage(Ovo_face);
     }
 
     //Robot moves left when left arrow is pressed
